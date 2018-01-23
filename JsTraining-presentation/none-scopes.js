b03d745338304ae87458225ad904e8f07d7da219
Krzysztof Danielewicz <kdanielewicz@gmail.com>
2018-01-24 00:37:16 +0100

    new version

--- JsTraining-presentation/none-scopes.js
+++
@@ -1,8 +0,0 @@
-var temp = 'outer';
-
-for (var index = 0; index < 10; index++) {
-    
-    var temp = 'inner'; 
-}
-
-console.log(temp);
--- JsTraining-presentation/scopes.js
+++
@@ -1,9 +0,0 @@
-var temp = 'outer';
-
-function f1(){
-    var temp = 'inner';
-}
-
-f1();
-
-console.log(temp);
